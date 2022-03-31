7f3eed1b480758242030a7820c47f7191a469fdf
Daria Artemova <airadavometra@gmail.com>
2022-03-31 14:36:46 +0300

    add photo page

---
+++ pages/photo.tsx
@@ -0,0 +1,6 @@
+import s from "@/pagesStyles/Photo.module.css";
+import type { NextPage } from "next";
+
+const PhotoPage: NextPage = () => <div className={s.main}></div>;
+
+export default PhotoPage;
