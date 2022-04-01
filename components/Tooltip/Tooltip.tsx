d85423e18a7acf280d24ae17c6aee9fd61c10ad8
Daria Artemova <airadavometra@gmail.com>
2022-04-01 17:22:07 +0300

    add skills tooltip

---
+++ components/Tooltip/Tooltip.tsx
@@ -0,0 +1,30 @@
+import classNames from "classnames";
+import React, { FC, useState } from "react";
+import classes from "./Tooltip.module.css";
+
+type TooltipProps = {
+  text: string;
+};
+
+export const Tooltip: FC<TooltipProps> = ({ children, text }) => {
+  const [show, setShow] = useState(false);
+
+  return (
+    <div className={classes.tooltipContainer}>
+      <div
+        className={classNames(classes.tooltipBox, {
+          [classes.visible]: show,
+        })}
+      >
+        {text}
+      </div>
+      <div
+        className={classes.childrenContainer}
+        onMouseEnter={() => setShow(true)}
+        onMouseLeave={() => setShow(false)}
+      >
+        {children}
+      </div>
+    </div>
+  );
+};
