5da2d30548507964d0860a2d4719ad3d3194c0a5
Daria Artemova <airadavometra@gmail.com>
2022-04-01 22:48:05 +0300

    add photo hover effect

--- components/PhotoItem/PhotoItem.tsx
+++ components/PhotoItem/PhotoItem.tsx
@@ -2,6 +2,7 @@ import React, { FunctionComponent } from "react";
 import s from "./PhotoItem.module.css";
 import classNames from "classnames";
 import Image from "next/image";
+import { motion } from "framer-motion";
 
 export interface PhotoItemProps {
   cssClassNames: string;
@@ -17,18 +18,28 @@ export const PhotoItem: FunctionComponent<PhotoItemProps> = ({
   onClick,
 }) => {
   return (
-    <div className={classNames(s.photoContainer, cssClassNames)}>
-      <Image
-        onContextMenu={(e) => e.preventDefault()}
-        src={src}
-        onClick={() => onClick(imgId)}
-        alt="photo"
-        loading="lazy"
-        //placeholder="blur" Нельзя нормально использовать с loading="lazy", подумать над генерацией плейсхолдеров, чтобы задать их через ссылки явно
-        quality={100}
-        layout="fill"
-        objectFit="cover"
-      />
-    </div>
+    <motion.div
+      className={classNames(s.photoOuterContainer, cssClassNames)}
+      whileHover={{ scale: 0.95 }}
+      transition={{ type: "spring", stiffness: 120, duration: 1.5 }}
+    >
+      <motion.div
+        className={classNames(s.photoContainer, cssClassNames)}
+        whileHover={{ scale: 1.15 }}
+        transition={{ type: "spring", stiffness: 120, duration: 1.5 }}
+      >
+        <Image
+          onContextMenu={(e) => e.preventDefault()}
+          src={src}
+          onClick={() => onClick(imgId)}
+          alt="photo"
+          loading="lazy"
+          //placeholder="blur" Нельзя нормально использовать с loading="lazy", подумать над генерацией плейсхолдеров, чтобы задать их через ссылки явно
+          quality={100}
+          layout="fill"
+          objectFit="cover"
+        />
+      </motion.div>
+    </motion.div>
   );
 };
