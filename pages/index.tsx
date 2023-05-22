04012477a12b82d2a0de1ec4e8e9c4ed75db7160
Daria Artemova <airadavometra@gmail.com>
2023-05-22 18:50:20 +0200

    small fixes

--- pages/index.tsx
+++ pages/index.tsx
@@ -10,7 +10,7 @@ const Home: NextPage = () => {
   return (
     <div className={s.main}>
       <div className={s.description}>
-        <h2 className={s.textHeader}>Hey. I’m Daria,</h2>
+        <h2 className={s.textHeader}>Hey. I&apos;m Daria,</h2>
         <p className={s.text}>
           <b>
             a passionate front-end developer <br /> based in Amsterdam, The
@@ -18,16 +18,17 @@ const Home: NextPage = () => {
           </b>
         </p>
         <p className={s.text}>
-          Besides my love for web development, I’m also interested in design,
-          especially UX, and I’m working on improving my designer skills.
+          Besides my love for web development, I&apos;m also interested in
+          design, especially UX, and I&apos;m working on improving my designer
+          skills.
         </p>
         <p className={s.text}>
-          When I’m not coding, you can catch me outside exploring, taking
+          When I&apos;m not coding, you can catch me outside exploring, taking
           photos, flying drone, hiking. I just adore nature and hope someday to
           see the whole world.
         </p>
         <div className={s.btnsContainer}>
-          <Link key="portfolio" href="/portfolio">
+          <Link key="portfolio" href="/portfolio" passHref>
             <motion.a
               className={s.accentBtn}
               variants={menuItemVariants}
@@ -36,7 +37,7 @@ const Home: NextPage = () => {
               See my portfolio
             </motion.a>
           </Link>
-          <Link key="contacts" href="/contact">
+          <Link key="contacts" href="/contact" passHref>
             <motion.a
               className={s.secondaryBtn}
               variants={menuItemVariants}
