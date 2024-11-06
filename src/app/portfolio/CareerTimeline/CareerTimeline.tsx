ad3c8fba7d56ccbef09ab729b77722149d76ba18
Daria Artemova <airadavometra@gmail.com>
2024-11-06 22:57:32 +0100

    replace radix with headless ui

--- src/app/portfolio/CareerTimeline/CareerTimeline.tsx
+++ src/app/portfolio/CareerTimeline/CareerTimeline.tsx
@@ -1,18 +1,20 @@
 "use client";
 
+import { Fragment, useEffect, useState } from "react";
 import s from "./CareerTimeline.module.css";
 import { CAREER_TIMELINE } from "@/constants/careerTimeline";
-import * as Accordion from "@radix-ui/react-accordion";
 import { Expand } from "@/icons/Expand";
+import {
+  Disclosure,
+  DisclosureButton,
+  DisclosurePanel,
+} from "@headlessui/react";
 import { motion, AnimatePresence } from "framer-motion";
 import {
   expandExperienceItem,
   experienceButtonVariants,
 } from "@/motions/portfolio";
-import { useEffect, useState } from "react";
-
-const AccordionContent = motion.create(Accordion.Content);
-const AccordionHeader = motion.create(Accordion.Header);
+import classNames from "classnames";
 
 export const CareerTimeline = () => {
   const [defaultValue, setDefaultValue] = useState<string[]>();
@@ -24,45 +26,47 @@ export const CareerTimeline = () => {
   return (
     <section className={s.section}>
       <h1 className={s.sectionTitle}>Key Points</h1>
-      <Accordion.Root
-        key={defaultValue ? defaultValue[0] : "accordion"}
-        type="multiple"
-        defaultValue={defaultValue}
-        className={s.experienceList}
-      >
-        {CAREER_TIMELINE.map((item) => (
-          <Accordion.Item
-            key={item.title}
-            value={item.title}
-            className={s.experienceItem}
-          >
-            <AccordionHeader
-              whileHover="hover"
-              variants={experienceButtonVariants}
-            >
-              <Accordion.Trigger className={s.experienceItemTrigger}>
-                <span className={s.year}>{item.year}</span>
-                <span>{item.title}</span>
-                <div className={s.expandBtnWrapper}>
-                  <Expand className={s.expandBtn} aria-hidden />
-                </div>
-              </Accordion.Trigger>
-            </AccordionHeader>
-            <Accordion.Content>
-              <AnimatePresence>
-                <motion.div
-                  initial="hidden"
-                  animate="visible"
-                  exit="exit" // TODO: fix exit animation, it doesn't work now
-                  variants={expandExperienceItem}
+      <div className={s.experienceList}>
+        {CAREER_TIMELINE.map((item, index) => (
+          <Disclosure as="div" key={item.title} defaultOpen={index === 0}>
+            {({ open }) => (
+              <>
+                <DisclosureButton
+                  className={s.experienceItemTrigger}
+                  as={motion.button}
+                  whileHover="hover"
+                  variants={experienceButtonVariants}
                 >
-                  <p className={s.experienceItemText}>{item.text}</p>
-                </motion.div>
-              </AnimatePresence>
-            </Accordion.Content>
-          </Accordion.Item>
+                  <span className={s.year}>{item.year}</span>
+                  <span>{item.title}</span>
+                  <div className={s.expandBtnWrapper}>
+                    <Expand
+                      className={classNames(s.expandBtn, {
+                        [s.rotate]: open,
+                      })}
+                      aria-hidden
+                    />
+                  </div>
+                </DisclosureButton>
+                <AnimatePresence>
+                  {open && (
+                    <DisclosurePanel static as={Fragment}>
+                      <motion.div
+                        initial="hidden"
+                        animate="visible"
+                        exit="exit"
+                        variants={expandExperienceItem}
+                      >
+                        <p className={s.experienceItemText}>{item.text}</p>
+                      </motion.div>
+                    </DisclosurePanel>
+                  )}
+                </AnimatePresence>
+              </>
+            )}
+          </Disclosure>
         ))}
-      </Accordion.Root>
+      </div>
     </section>
   );
 };
