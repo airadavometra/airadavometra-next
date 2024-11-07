58fe9239181b5a1dd880b44c4e460a5ab297393a
Daria Artemova <airadavometra@gmail.com>
2024-11-07 22:14:02 +0100

    add mobile menu

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,26 +1,16 @@
 "use client";
 
-import { useState } from "react";
 import s from "./Header.module.css";
 import classNames from "classnames";
 import { usePathname } from "next/navigation";
 import Link from "next/link";
-import { AnimatePresence, motion } from "framer-motion";
+import { motion } from "framer-motion";
 import { menuItemVariants } from "@/motions/header";
 import { NAVIGATION } from "@/constants/navigation";
-import { Burger } from "@/icons/Burger";
-import { toggleFreezePage } from "@/utils/toggleFreezePage";
-import { VisuallyHidden } from "../VisuallyHidden/VisuallyHidden";
+import { MobileMenu } from "../MobileMenu/MobileMenu";
 
 export const Header = () => {
   const pathname = usePathname();
-
-  const [isMenuOpen, setMenuOpen] = useState(false);
-
-  const handleToggleMobileMenu = () => {
-    setMenuOpen((prev) => !prev);
-    toggleFreezePage();
-  };
 
   return (
     <>
@@ -45,14 +35,8 @@ export const Header = () => {
             ))}
           </ul>
         </nav>
-        <button className={s.menuButton} onClick={handleToggleMobileMenu}>
-          <Burger className={s.menuIcon} />
-          <VisuallyHidden>Open menu</VisuallyHidden>
-        </button>
+        <MobileMenu />
       </header>
-      <AnimatePresence mode="wait">
-        {isMenuOpen && <>Mobile menu open</>}
-      </AnimatePresence>
     </>
   );
 };
--- src/icons/Close.tsx
+++ src/icons/Close.tsx
@@ -6,15 +6,15 @@ export const Close: FC<WithClassName> = ({ className }) => {
     <svg
       className={className}
       xmlns="http://www.w3.org/2000/svg"
-      fill="none"
       viewBox="0 0 24 24"
-      strokeWidth={2}
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
     >
-      <path
-        strokeLinecap="round"
-        strokeLinejoin="round"
-        d="M6 18L18 6M6 6l12 12"
-      />
+      <line x1="18" y1="6" x2="6" y2="18"></line>
+      <line x1="6" y1="6" x2="18" y2="18"></line>
     </svg>
   );
 };
