545aadcd78b2cdb4fcfd7ccf8147d78550c30e5d
Daria Artemova <airadavometra@gmail.com>
2024-11-10 21:57:42 +0100

    fix sticky header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -14,6 +14,27 @@ export const Header = () => {
 
   return (
     <>
+      {
+        // Duplicate header (with aria-hidden and opacity 0) to take space. Because real header has position fixed and doesn't take that space in the layout
+      }
+      <div className={classNames(s.header, s.headerHidden)} aria-hidden>
+        <div>
+          <div className={s.navigation}>
+            {NAVIGATION.map(({ title, path }) => (
+              <div key={path}>
+                <div
+                  className={classNames(s.link, {
+                    [s.selected]: path === pathname,
+                  })}
+                >
+                  {title}
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
+        <MobileMenu />
+      </div>
       <header className={s.header}>
         <nav>
           <ul className={s.navigation}>
